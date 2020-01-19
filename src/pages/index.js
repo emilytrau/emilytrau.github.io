aeff526325042f14dfb38cc2b24722e0abbe4176
Angus Trau <me@angus.ws>
2020-01-19 19:47:01 +1100

    Disable the blog section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
 import Projects from '../sections/Projects';
-import Blog from '../sections/Blog';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -13,7 +12,6 @@ const IndexPage = () => (
     <Landing />
     <About />
     <Projects />
-    <Blog />
     <Footer />
   </Layout>
 );
